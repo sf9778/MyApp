1e55295a1c3f29abd219b3792fd720125cf930f8
sf9778 <sf9778@student.uni-lj.si>
2023-01-08 22:44:38 +0100

    Initial commit.

--- app/src/main/java/com/example/myapp/AddDelavecActivity.java
+++ app/src/main/java/com/example/myapp/AddDelavecActivity.java
@@ -3,6 +3,7 @@ package com.example.myapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -11,13 +12,21 @@ import com.android.volley.RequestQueue;
 
 public class AddDelavecActivity extends AppCompatActivity {
     private TextView status;
-    private EditText ime;
-    private EditText priimek;
+    private EditText name;
+    private EditText surname;
     private RequestQueue requestQueue;
+    private String url = "https://knjigovodstvo.azurewebsites.net/api/v1/delavci";
+
+    public void addDelavec(View view){
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_delavec);
+        name = (EditText) findViewById(R.id.ime);
+        surname = (EditText) findViewById(R.id.priimek);
+        status = (TextView) findViewById(R.id.stanje);
     }
 }
