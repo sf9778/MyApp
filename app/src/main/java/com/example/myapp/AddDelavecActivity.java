e3287eef32df23b5d02b3273d1b265fc3db95970
sf9778 <sf9778@student.uni-lj.si>
2023-01-08 22:26:52 +0100

    Initial commit.

--- app/src/main/java/com/example/myapp/AddDelavecActivity.java
+++ app/src/main/java/com/example/myapp/AddDelavecActivity.java
@@ -3,8 +3,17 @@ package com.example.myapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.android.volley.RequestQueue;
 
 public class AddDelavecActivity extends AppCompatActivity {
+    private TextView status;
+    private EditText ime;
+    private EditText priimek;
+    private RequestQueue requestQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
